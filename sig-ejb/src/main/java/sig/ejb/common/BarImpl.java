ed18a5fe12ff8b9cc7d8c87e6899c57100c7233a
bug-pc\bog <luis.condori.c@gmail.com>
2017-08-04 12:55:07 -0500

    java delegate procees diagramas

--- sig-ejb/src/main/java/sig/ejb/common/BarImpl.java
+++ sig-ejb/src/main/java/sig/ejb/common/BarImpl.java
@@ -2,11 +2,16 @@ package sig.ejb.common;
 
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.example.Blog;
 import org.mybatis.example.BlogMapper;
+
+import com.sig.camunda.bpm_lib.CamundaEngineLib;
 
 import sig.ejb.common.Bar;
 import sig.mybatis.MyBatisUtil;
@@ -31,6 +36,13 @@ public class BarImpl implements Bar {
             System.out.println("ERROR");
         }
 
+        
+    	Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("var", "2");
+		CamundaEngineLib a = new CamundaEngineLib();
+		String as = a.processCreate("Process_1", "", "", "", variables);
+		
+        
 		return "mensaje desde ejb con";
 	}
 
---
+++ sig-rest/src/main/java/org/sig/servlet/EjbJavaDelegate.java
@@ -0,0 +1,42 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sig.servlet;
+
+import java.util.logging.Logger;
+
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+
+/**
+ * This is an Ejb which is invoked form a Service task
+ *
+ */
+@Named("ejbSaludo")
+@Stateless
+public class EjbJavaDelegate implements JavaDelegate {
+
+  private final static Logger LOGGER = Logger.getLogger(EjbJavaDelegate.class.getName());
+  
+
+  public void execute(DelegateExecution execution) throws Exception {
+
+		LOGGER.info("\n\n\n Este ejemplo corre del ejb \n\n\n");
+
+	}
+
+}
