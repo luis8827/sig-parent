190b8140aca0230709f3a2b3f84415f14be3e44a
bug-pc\bog <luis.condori.c@gmail.com>
2017-08-10 14:42:00 -0500

    respuestaDTO. servicios de usuaricontroller

--- sig-rest/src/main/java/sig/rest/controller/UsuariosController.java
+++ sig-rest/src/main/java/sig/rest/controller/UsuariosController.java
@@ -14,9 +14,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import sig.camunda.ejb.UsuariosEJB;
+
 import sig.camunda.ejb.UsuariosInterface;
-import sig.ejb.dto.procesoDTO;
+
 import sig.rest.dto.UsuarioDTO;
 
 @Stateless
--- sig-rest/src/main/java/sig/rest/dto/RespuestaDTO.java
+++ sig-rest/src/main/java/sig/rest/dto/RespuestaDTO.java
@@ -5,4 +5,24 @@ import java.util.Map;
 public class RespuestaDTO {
 	private boolean success;
 	private Map<String, Object> variables;
+	public RespuestaDTO(boolean success, Map<String, Object> variables) {
+		super();
+		this.success = success;
+		this.variables = variables;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
+	public void setVariables(Map<String, Object> variables) {
+		this.variables = variables;
+	}
+	
+	
+	
 }
